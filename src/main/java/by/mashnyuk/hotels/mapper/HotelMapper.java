942799b506abec31c4a27261abc9866c8d52a163
Darya Mashniuk <darya.m2006.alesya@gmail.com>
2026-09-13 00:11:37 +0200

    feat(mapper): create MapStruct mappers for Hotel entities and DTOs

---
+++ src/main/java/by/mashnyuk/hotels/mapper/HotelMapper.java
@@ -0,0 +1,77 @@
+package by.mashnyuk.hotels.mapper;
+
+import by.mashnyuk.hotels.model.Address;
+import by.mashnyuk.hotels.model.Amenity;
+import by.mashnyuk.hotels.model.ArrivalTime;
+import by.mashnyuk.hotels.model.Contacts;
+import by.mashnyuk.hotels.model.Hotel;
+import by.mashnyuk.hotels.model.dto.AddressDto;
+import by.mashnyuk.hotels.model.dto.ArrivalTimeDto;
+import by.mashnyuk.hotels.model.dto.ContactsDto;
+import by.mashnyuk.hotels.model.dto.CreateHotelDto;
+import by.mashnyuk.hotels.model.dto.HotelFullDto;
+import by.mashnyuk.hotels.model.dto.HotelShortDto;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
+
+import java.util.Collections;
+import java.util.List;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        builder = @Builder(disableBuilder = true)
+)
+public interface HotelMapper {
+
+    @Mapping(target = "address", source = "address", qualifiedByName = "formatAddressString")
+    @Mapping(target = "phone", source = "contacts.phone")
+    HotelShortDto toShortDto(Hotel hotel);
+
+    List<HotelShortDto> toShortDtoList(List<Hotel> hotels);
+
+    @Mapping(target = "amenities", source = "amenities", qualifiedByName = "mapAmenitiesToStrings")
+    HotelFullDto toFullDto(Hotel hotel);
+
+    AddressDto toAddressDto(Address address);
+
+    ContactsDto toContactsDto(Contacts contacts);
+
+    ArrivalTimeDto toArrivalTimeDto(ArrivalTime arrivalTime);
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "amenities", ignore = true)
+    Hotel toEntity(CreateHotelDto dto);
+
+    Address toAddressEntity(AddressDto dto);
+
+    Contacts toContactsEntity(ContactsDto dto);
+
+    ArrivalTime toArrivalTimeEntity(ArrivalTimeDto dto);
+
+    @Named("formatAddressString")
+    default String formatAddressString(Address address) {
+        if (address == null) {
+            return null;
+        }
+        return String.format("%d %s, %s, %s, %s",
+                address.getHouseNumber(),
+                address.getStreet(),
+                address.getCity(),
+                address.getPostCode(),
+                address.getCountry());
+    }
+
+    @Named("mapAmenitiesToStrings")
+    default List<String> mapAmenitiesToStrings(List<Amenity> amenities) {
+        if (amenities == null) {
+            return Collections.emptyList();
+        }
+        return amenities.stream()
+                .map(Amenity::getName)
+                .toList();
+    }
+}
